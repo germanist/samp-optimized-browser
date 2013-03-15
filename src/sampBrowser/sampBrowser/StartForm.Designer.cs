232631812b2ae6370ca74b53e46c0d0edfe3925b
samp.extensions@gmail.com <samp.extensions@gmail.com@c4cc68aa-6221-5c00-4af5-468e1a9dfdbc>
2013-03-15 18:08:21 +0000

    main src

---
+++ src/sampBrowser/sampBrowser/StartForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace sampBrowser
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgWork = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.serverListGrid = new System.Windows.Forms.DataGridView();
+            this.serverRules = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.serverListGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.serverRules)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bgWork
+            // 
+            this.bgWork.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWork_DoWork);
+            this.bgWork.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWork_ProgressChanged);
+            this.bgWork.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWork_RunWorkerCompleted);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 379);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(626, 129);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 22);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(635, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 350);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(626, 23);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Favorites";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Internet";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // serverListGrid
+            // 
+            this.serverListGrid.AllowUserToAddRows = false;
+            this.serverListGrid.AllowUserToDeleteRows = false;
+            this.serverListGrid.AllowUserToResizeRows = false;
+            this.serverListGrid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.serverListGrid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.serverListGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.serverListGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.serverListGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.serverListGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.serverListGrid.Location = new System.Drawing.Point(3, 3);
+            this.serverListGrid.MultiSelect = false;
+            this.serverListGrid.Name = "serverListGrid";
+            this.serverListGrid.ReadOnly = true;
+            this.serverListGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.serverListGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.serverListGrid.ShowEditingIcon = false;
+            this.serverListGrid.Size = new System.Drawing.Size(626, 341);
+            this.serverListGrid.TabIndex = 0;
+            this.serverListGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.serverListGrid_CellClick);
+            // 
+            // serverRules
+            // 
+            this.serverRules.AllowUserToAddRows = false;
+            this.serverRules.AllowUserToDeleteRows = false;
+            this.serverRules.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.serverRules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.serverRules.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.serverRules.Location = new System.Drawing.Point(635, 379);
+            this.serverRules.Name = "serverRules";
+            this.serverRules.ReadOnly = true;
+            this.serverRules.Size = new System.Drawing.Size(228, 129);
+            this.serverRules.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.02551F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.97448F));
+            this.tableLayoutPanel1.Controls.Add(this.serverListGrid, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.serverRules, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 68.21862F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.870445F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.11336F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(866, 511);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 511);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "StartForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.StartForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.serverListGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.serverRules)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgWork;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        public System.Windows.Forms.DataGridView serverListGrid;
+        private System.Windows.Forms.DataGridView serverRules;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+    }
+}
+
